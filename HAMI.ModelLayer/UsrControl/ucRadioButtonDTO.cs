b5367c067755d5d6cdd1554cd319e4087c47bba0
CoulsonChen <a0936792910@gmail.com>
2016-12-26 17:30:09 +0800

    Modify

---
+++ HAMI.ModelLayer/UsrControl/ucRadioButtonDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAMI.ModelLayer.UsrControl
+{
+    public class ucRadioButtonDTO
+    {
+        /// <summary>
+        /// Item序號 (對應使用者KeyIn的號碼)
+        /// </summary>
+        public int ItemNo { get; set; }
+
+        /// <summary>
+        /// 畫面顯示字樣
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// SelectValue
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
